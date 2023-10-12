1b8536cadf300861dbbb4c1772dc363493a1a7cd
EwenJezCNAM <r1bo.gamedesign@gmail.com>
2023-10-12 17:05:07 +0200

    programme avec la led de fin qui bug

--- src/main.cpp
+++ src/main.cpp
@@ -16,27 +16,51 @@
 
 int BUTTONS[numLeds] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6};
 int LEDS[numLeds] = {LED1, LED2, LED3, LED4, LED5, LED6};
-
+bool pushed = true;
 
 // put function declarations here:
 // int myFunction(int, int);
 
 void setup() {
-
+  randomSeed(analogRead(0));
   for (int i = 0; i < numLeds; i++)
   {
+    long light = random(2);
     pinMode(BUTTONS[i], INPUT_PULLUP);
     pinMode(LEDS[i], OUTPUT);
+    if (light){
+      digitalWrite(LEDS[i], HIGH);
+    }
+    else{
+      digitalWrite(LEDS[i], LOW);
+    }
   }
 
 }
 
 void loop() {
-
   for (int i = 0; i < numLeds; i++)
   {
     bool buttonPressed = digitalRead(BUTTONS[i]);
-    digitalWrite(LEDS[i], !buttonPressed);
+    if (buttonPressed & !pushed){
+      pushed = !pushed;
+      if(i == 0){
+        digitalWrite(LEDS[0], !digitalRead(LEDS[0]));
+        digitalWrite(LEDS[1], !digitalRead(LEDS[1]));
+      }
+      if (i == numLeds){
+        digitalWrite(LEDS[1], !digitalRead(LEDS[1]));
+        digitalWrite(LEDS[2], !digitalRead(LEDS[2]));
+      }
+      else{
+        digitalWrite(LEDS[i-2], !digitalRead(LEDS[i-2]));
+        digitalWrite(LEDS[i-1], !digitalRead(LEDS[i-1]));
+        digitalWrite(LEDS[i], !digitalRead(LEDS[i]));
+      }
+    }
+    if (!buttonPressed & pushed){
+      pushed = !pushed;
+    }
   }
-
+  
 }
