7e56fa384aae981a2b169c289f9a427872b0edf6
EwenJezCNAM <r1bo.gamedesign@gmail.com>
2023-10-19 16:15:16 +0200

    function checkLedsState

--- src/dev-colin(copi).cpp
+++ src/dev-colin(copi).cpp
@@ -14,9 +14,8 @@
 #define LED6 13
 #define numLeds 6
 
-
-int BUTTONS[numLeds] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6};
-int LEDS[numLeds] = {LED1, LED2, LED3, LED4, LED5, LED6};
+const int BUTTONS[numLeds] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6};
+const int LEDS[numLeds] = {LED1, LED2, LED3, LED4, LED5, LED6};
 
 bool ledState[numLeds];
 bool buttonState[numLeds];
@@ -25,26 +24,17 @@ bool lastButtonState[numLeds];
 unsigned long lastDebounceTime[numLeds];
 const unsigned long debounceDelay = 50;
 
-// put function declarations here:
-// int myFunction(int, int);
 
+// put function declarations here:
 void randomizeLedStates();
-void refreshLeds();
+
 
 void setup() {
-  Serial.begin(115200);
-  randomSeed(analogRead(0));
+
   for (int i = 0; i < numLeds; i++)
   {
-    long light = random(2);
     pinMode(BUTTONS[i], INPUT_PULLUP);
     pinMode(LEDS[i], OUTPUT);
-    if (light){
-      digitalWrite(LEDS[i], HIGH);
-    }
-    else{
-      digitalWrite(LEDS[i], LOW);
-    }
     lastButtonState[i] = false;
     lastDebounceTime[i] = 0;
   }
@@ -53,13 +43,13 @@ void setup() {
 
 }
 
-void loop() {
-  for (int i = 0; i < numLeds; i++){
 
-  }
+void loop() {
+
   for (int i = 0; i < numLeds; i++)
   {
     bool currentButtonPressed = digitalRead(BUTTONS[i]);
+
     if (currentButtonPressed != lastButtonState[i]) {
       lastDebounceTime[i] = millis();
     }
@@ -68,51 +58,24 @@ void loop() {
       if (currentButtonPressed != buttonState[i]) {
         buttonState[i] = currentButtonPressed;
         if (currentButtonPressed == true) {
-          if(i == 0){
-            ledState[0] = !ledState[0];
-            ledState[1] = !ledState[1];
-          }
-          else if (i == numLeds-1){
-            ledState[numLeds-2] = !ledState[numLeds-2];
-            ledState[numLeds-1] = !ledState[numLeds-1];
-          }
-          else{
-            ledState[i-1] = !ledState[i-1];
-            ledState[i] = !ledState[i];
-            ledState[i+1] = !ledState[i+1];
-          }
+          ledState[i] = !ledState[i];
         }
       }
     }
 
     lastButtonState[i] = currentButtonPressed;
-    refreshLeds();
+
+    digitalWrite(LEDS[i], !ledState[i]);
   }
 }
 
+
 void randomizeLedStates() {
+
   for (int i = 0; i < numLeds; i++)
   {
     randomSeed(analogRead(0));
     ledState[i] = random(0, 2);
   }
-}
 
-void refreshLeds(){
-  for (int i = 0; i < numLeds; i++){
-    digitalWrite(LEDS[i], ledState[i]);
-  }
-}
-
-bool checkLedsState(){
-  int totalLedsOn = 0;
-  for (int i = 0; i < numLeds; i++){
-    totalLedsOn += ledState[i];
-  }
-  if (totalLedsOn == numLeds){
-    return true;
-  }
-  else{
-    return false;
-  }
 }
